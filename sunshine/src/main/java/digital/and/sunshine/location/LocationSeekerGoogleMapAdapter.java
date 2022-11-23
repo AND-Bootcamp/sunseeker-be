b71f3c01bbd0d099861498882454bea6ae108613
hippalus <habiphakan.isler@gmail.com>
2022-11-23 17:28:38 +0100

    BSS-50 Integrate GoogleMap NearPlace Api

--- sunshine/src/main/java/digital/and/sunshine/location/LocationSeekerGoogleMapAdapter.java
+++ sunshine/src/main/java/digital/and/sunshine/location/LocationSeekerGoogleMapAdapter.java
@@ -1,14 +1,61 @@
 package digital.and.sunshine.location;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "location-seeker", name = "google-map.enabled", havingValue = "true")
-public class LocationSeekerGoogleMapAdapter implements LocationSeekerService{
+public class LocationSeekerGoogleMapAdapter implements LocationSeekerService {
 
   @Override
-  public Places getNearPlaces(RetrieveNearPlaces retrieveNearPlaces) {
-    return null;
+  public Places getNearPlaces(final RetrieveNearPlaces retrieveNearPlaces) {
+    try {
+      final GeoApiContext context = new GeoApiContext.Builder()
+          .apiKey("a6905bec531bcc6b8cebf3169540bc24")
+          //.apiKey("AIzaSyBXxJFzycmI4A6vWVQaSCj4b7jB22H9-fI")
+          .build();
+
+      final LatLng latLng = this.getLatLng(retrieveNearPlaces);
+
+      final PlacesSearchResponse placesSearchResponse = PlacesApi.nearbySearchQuery(context, latLng)
+          .radius(retrieveNearPlaces.radiusInMeter())
+          .await();
+
+      final List<Coordinates> coordinatesList = Arrays.stream(placesSearchResponse.results)
+          .map(this::getCoordinates)
+          .toList();
+
+      context.shutdown();
+
+      return new Places(coordinatesList);
+    } catch (final Exception e) {
+      log.error("Exception has been occurred ", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  @NotNull
+  private LatLng getLatLng(final RetrieveNearPlaces retrieveNearPlaces) {
+    final Coordinates coordinates = retrieveNearPlaces.coordinates();
+
+    return new LatLng(coordinates.lat(), coordinates.lon());
+  }
+
+  private Coordinates getCoordinates(final PlacesSearchResult placesSearchResult) {
+    final LatLng location = placesSearchResult.geometry.location;
+
+    return Coordinates.of(location.lng, location.lat);
   }
 }
